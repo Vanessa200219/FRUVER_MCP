dd5ad32ef484d180fb694021bde25e7d56547e21
vamendoza <vamendoza19@misena.edu.co>
2021-10-22 18:00:28 -0500

    menu de index

---
+++ indexPrincipal.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="CSSindex/index.css">
+  <title>FRUVER MCP</title>
+</head>
+
+<body>
+  <!-- Menu -->
+    <section id="header">
+        <div class="header">
+            <div class="nav-bar">
+                <nav class="nav-list">
+                    <div class="hamburger">
+                        <div class="bar"></div>
+                    </div>
+                            <div class="img">
+                            <img class="logo" src="IMG/logo.png" align="" alt="">
+                            </div>
+                        <ul>
+                            <li><a href="#" data-after="Home"><strong>INICIO</strong></a></li>
+                            <li><a href="#" data-after="Service"><strong>SOMOS</strong></a></li>
+                            <li><a href="#" data-after="Politics"><strong>POLITICAS</strong></a></li>
+                            <li><a href="#" data-after="Contact"><strong>CONTACTOS</strong></a></li>
+                        </ul>
+                </nav>
+            </div>
+        </div>
+    </section>
+  <!-- FIN MENU -->
+
+
+  <!-- Portada-->
+    <section>
+
+    </section>
+
+
+
+
+  <!-- PORTADA 
+  <section id="hero">
+    <div class="hero container">
+      <div>
+        <h1>DESARROLLO DE APLICACIONES WEB</h1>
+        <h3>Desarrollador de Codigo front-end y back-end, Programador de JavaScript y PHP.<span></span></h3>
+      </div>
+    </div>
+  </section> -->
+  <!-- FIN PORTADA -->
+
+
+
+<!-- Biografia -->
+<!-- <div id="informacion"><section class="Biografia">
+    <h1 class="subtitle">TECNOLOGO EN SISTEMAS DE INFORMACION</h1>
+        <p class="presentation__copy">Analista de Sistema de Informacion, Programadora de Software y Mantenimiento en Programas.</p>
+        <a href="hojadevida.php" type="button" class="cta">HOJA DE VIDA</a>
+</section></div> -->
+<!-- FIN  BIOGRAFIA -->
+
+
+
+
+
+
+  <!-- PROYECTO -->
+  <!-- <section id="services">
+    <div class="services">
+      <div class="service-top">
+        <div id="Proyectos"><h1 class="section-title">Proyectos</h1></div>
+
+      
+        <div class="service-bottom">
+
+            <div class="service-item">
+                <div class="color">
+                    <h2>MVC</h2>
+                    <br>
+                </div>
+                <a class="vinculo" href="../MVCDAO/Vista/">Ver Sitio</a>
+            </div>
+
+
+            <div class="service-item">
+                <div class="color">
+                    <h2>JAVASCRIPT</h2>
+                    <br>
+                </div>
+                <a class="vinculo" href="JavaScript/java.php">Ver Sitio</a>
+            </div>
+
+
+            <div class="service-item">
+                <div class="color">
+                    <h2>JAVASCRIPT HERENCIA</h2>
+                    <br>
+                </div>
+                <a class="vinculo" href="JavaScript/HerenciaProyecto/herencia.php">Ver Sitio</a>
+            </div>
+
+        
+        </div>
+
+
+      </div> 
+    </div> -->
+  </section>
+  <!-- PROYECTO -->
+
+  <!-- MANUALES -->
+  <!-- <section id="projects">
+    <div class="projects container">
+      <div class="projects-header">
+        <div id="Manuales"><h1 class="section-title">MANUALES</h1></div>
+      </div>
+      
+      <div class="all-projects">
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Manual de Usuarios</h1>
+            <p>
+                Una Guía de usuario, también conocida como Manual de usuario, es un documento de comunicación 
+                técnica destinado a dar asistencia a las personas que utilizan un sistema en particular
+            </p>
+            <br>
+            <a class="vinculo" href="Manual/manualdeusuarios.php">Ver Sitio</a>
+          </div>
+
+          
+          <div class="project-info">
+          <h1>Manual de Softwate</h1>
+            <p>
+                Documento que contiene información válida y clasificada sobre una determinada materia de la 
+                organización. Es un compendio, una colección de textos seleccionados y fácilmente localizables.
+            </p>
+            <br>
+            <a class="vinculo" href="Manual/manualdesoftware.php">Ver Sitio</a>
+          </div>
+        </div>
+
+
+        </div>
+      </div>
+      </div>
+    </div>
+  </section> -->
+  <!-- FIN MANUALES -->
+
+  
+
+  <!-- Contactos -->
+  <!-- <section id="contact">
+    <div class="contact container">
+      <div>
+        <h1 class="section-title">CONTACTO</h1>
+      </div>
+      <div class="contact-items">
+        <div class="contact-item">
+          <div class="contact-info">
+            <h1>Telefono</h1>
+            <h2>+57 3202883867</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="contact-info">
+            <h1>Email</h1>
+            <h2>vamendoza19@misena.edu.co</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="contact-info">
+            <h1>Ubicacion</h1>
+            <h2>Cundinamarca, Soacha</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section> -->
+  <!-- FIN CONTACTO -->
+
+  <!-- Footer -->
+  <!-- <section id="footer">
+    <div class="footer container">
+      <div class="brand">
+        <h1>SITIO WEB</h1>
+      </div>
+        <h2>2021</h2>
+    </div>
+        
+      
+  </section> -->
+  <!-- End Footer -->
+  <script src="estilo.js"></script>
+</body>
+
+</html>
