2007492e51c41f591124862a2468c2f0690653f1
bdmerchan6 <bdmerchan6@misena.edu.co>
2021-11-02 08:59:06 -0500

    estilospagos

--- Formas_Pago/Formapago.php
+++ Formas_Pago/Formapago.php
@@ -1,6 +1,7 @@
 <?php
 
 require("../../FRUVER_MCP/Cuenta/Conexion/Conexion.php");
+//require("../indexPortada.php");//
 
 ?>
 
@@ -11,19 +12,54 @@ require("../../FRUVER_MCP/Cuenta/Conexion/Conexion.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formas de Pago</title>
+    <link rel="stylesheet" href="../Formas_Pago/css/estilospagos.css">
 </head>
 <body>
+    <section id="header">
+        <div class="header">
+            <div class="brand">
+                <a href="../indexPortada.php">
+                    <img src="../imgindex/logo.png" alt="" class="brand">
+                </a>
+            </div>
+            <div class="nav-bar">
+                <div class="nav-list">
+                <div class="hamburger">
+                <div class="bar"></div>
+            </div>
+            <ul>
+                <li><a href="#" data-after="Inicio">Inicio</a></li>
+                <li><a href="#services" data-after="Informacion">Informacion</a></li>
+                <li><a href="#about" data-after="Productos">Productos</a></li>
+                <li><a href="#contact" data-after="Contactos">Contactos</a></li>
+                <li><a href="Cuenta/Vista/iniciarsesion.php" data-after="Inicio sesion/Registrarse">Inicio sesion/Registrarse</a></li>
+            </ul>
+            </div>
+            </div>
+        </div>
+    </section>
+    <br>
+    <br>
+    <br>
+    <br>
     <div>
-        <a href="tarjeta_credito.php"><img src="../imgindex/tarjeta_credito.png" alt="MDN"></a>
+        <h1><center>SELECCIONE METODO DE PAGO</center></h1>
     </div>
     <div>
-        <a href=""><img src="../imgindex/tarjeta_debito.png" alt="MDN"></a>
+        <a href="tarjeta_credito.php"><img src="../Formas_Pago/img/tarjeta_credito.png" alt="MDN"></a>
+        <h3>Tarjeta de Credito</h3>
     </div>
     <div>
-        <a href=""><img src="../imgindex/efecty.png" alt="MDN"></a>
+        <a href=""><img src="../Formas_Pago/img/tarjeta_debito.png" alt="MDN"></a>
+        <h3>Tarjeta Debito</h3>
     </div>
     <div>
-        <a href=""><img src="../imgindex/contrareembolso.jpg" alt="MDN"></a>
+        <a href=""><img src="../Formas_Pago/img/efecty.png" alt="MDN"></a>
+        <h3>Punto de Efecty</h3>
+    </div>
+    <div>
+        <a href=""><img src="../Formas_Pago/img/contrareembolso.png" alt="MDN"></a>
+        <h3>Contra Reembolso</h3>
     </div>
 </body>
 </html>
