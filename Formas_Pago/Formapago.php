0cf2a5354052a95ba155a91e616b56f4b5c859e5
bdmerchan6 <bdmerchan6@misena.edu.co>
2021-11-02 07:27:32 -0500

    Formas de Pago

---
+++ Formas_Pago/Formapago.php
@@ -0,0 +1,29 @@
+<?php
+
+require("../../FRUVER_MCP/Cuenta/Conexion/Conexion.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formas de Pago</title>
+</head>
+<body>
+    <div>
+        <a href="tarjeta_credito.php"><img src="../imgindex/tarjeta_credito.png" alt="MDN"></a>
+    </div>
+    <div>
+        <a href=""><img src="../imgindex/tarjeta_debito.png" alt="MDN"></a>
+    </div>
+    <div>
+        <a href=""><img src="../imgindex/efecty.png" alt="MDN"></a>
+    </div>
+    <div>
+        <a href=""><img src="../imgindex/contrareembolso.jpg" alt="MDN"></a>
+    </div>
+</body>
+</html>
